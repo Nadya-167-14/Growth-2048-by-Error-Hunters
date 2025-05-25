0fcacdec862965363b243fc3e277358b431369aa
Nadya Shafwah Yusuf <nadya.123140167@student.itera.ac.id>
2025-05-26 00:50:44 +0700

    Update main.py : penambahan powerup

--- py/main.py
+++ py/main.py
@@ -1,8 +1,8 @@
 import pygame
 from game import Game2048
 from ui import draw_game, draw_game_over
-from mechanics import mechanics
-from menu import show_menu 
+from mechanics import mechanics_logic
+from menu import show_menu
 from powerup import pupuk, penyiram_otomatis, bom
 
 COLOR_TEXT = (50, 50, 50)
@@ -37,26 +37,56 @@ while running:
 
     pygame.display.flip()
 
+    # --- SENTRALISASI PENANGANAN EVENT DI SINI ---
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
-
+        
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            elif not game_over: # Hanya proses gerakan jika game belum over
+                if event.key == pygame.K_UP:
+                    game.move("up")
+                elif event.key == pygame.K_DOWN:
+                    game.move("down")
+                elif event.key == pygame.K_LEFT:
+                    game.move("left")
+                elif event.key == pygame.K_RIGHT:
+                    game.move("right")
+        
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-
-            if not game_over:
-                if powerup_buttons[0].collidepoint(mouse_pos):
-                    pupuk(game, 0, 0) 
-                elif powerup_buttons[1].collidepoint(mouse_pos):
-                    penyiram_otomatis(game)
-                elif powerup_buttons[2].collidepoint(mouse_pos):
-                    bom(game, 0, 0) 
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            
+            if not game_over: # Hanya proses power-up jika game belum over
+                if game.active_powerup is None:
+                    # Klik Tombol Power-up
+                    if powerup_buttons[0].collidepoint((mouse_x, mouse_y)):
+                        game.active_powerup = 'pupuk'
+                        print("Pupuk dipilih. Klik di papan untuk menggunakannya.")
+                    elif powerup_buttons[1].collidepoint((mouse_x, mouse_y)):
+                        penyiram_otomatis(game)
+                        game.active_powerup = None # Reset langsung jika tidak perlu klik kedua
+                    elif powerup_buttons[2].collidepoint((mouse_x, mouse_y)):
+                        game.active_powerup = 'bom'
+                        print("Bom dipilih. Klik di papan untuk meledakkannya.")
+                else:
+                    # Klik di Papan untuk Menggunakan Power-up Aktif
+                    row, col = game.get_board_coords((mouse_x, mouse_y))
+                    
+                    if row is not None and col is not None: # Jika klik berada di dalam papan
+                        if game.active_powerup == 'pupuk':
+                            pupuk(game, row, col)
+                        elif game.active_powerup == 'bom':
+                            bom(game, row, col)
+                        
+                        game.active_powerup = None # Reset power-up setelah digunakan
+                    else:
+                        print("Klik di luar papan, batalkan pemilihan power-up.")
+                        game.active_powerup = None # Batalkan power-up jika klik di luar papan
 
     if not game_over:
-        running = mechanics(game)
+        running = mechanics_logic(game) # Pastikan mechanics_logic mengembalikan True/False untuk running
 
     clock.tick(60)
 
