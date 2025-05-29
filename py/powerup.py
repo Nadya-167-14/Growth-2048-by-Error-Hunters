63310e66d9723f4dfbc545bd65b8c0784db43cd9
Nadya Shafwah Yusuf <nadya.123140167@student.itera.ac.id>
2025-05-29 08:27:16 +0700

    Update powerup.py

--- py/powerup.py
+++ py/powerup.py
@@ -1,8 +1,25 @@
+import pygame
+pygame.mixer.init()
+
+# Load sound effects
+powerup_sounds = {
+    "pupuk": pygame.mixer.Sound("assets/sfx/pupuk.wav"),
+    "penyiram_otomatis": pygame.mixer.Sound("assets/sfx/siram.wav"),
+    "bom": pygame.mixer.Sound("assets/sfx/bom.wav")
+}
+
+def play_powerup_sfx(powerup_name):
+    sound = powerup_sounds.get(powerup_name)
+    if sound:
+        sound.play()
+
+
 def pupuk(game_instance, row, col):
     if 0 <= row < game_instance.grid_size and 0 <= col < game_instance.grid_size:
         if game_instance.matrix[row][col] is not None:
             game_instance.matrix[row][col].value *= 2
-            game_instance.score += game_instance.matrix[row][col].value 
+            game_instance.score += game_instance.matrix[row][col].value
+            play_powerup_sfx("pupuk")
         else:
             return
     else:
@@ -10,8 +27,9 @@ def pupuk(game_instance, row, col):
 
 
 def penyiram_otomatis(game_instance):
-    game_instance.add_new_tile() 
+    game_instance.add_new_tile()
     game_instance.score += 10
+    play_powerup_sfx("siram")
 
 
 def bom(game_instance, row, col):
@@ -19,13 +37,14 @@ def bom(game_instance, row, col):
         if game_instance.matrix[row][col] is not None:
             removed_tile = game_instance.matrix[row][col]
             removed_value = removed_tile.value
-            
-            game_instance.matrix[row][col] = None 
-            
+
+            game_instance.matrix[row][col] = None
+
             if removed_tile in game_instance.tiles:
                 game_instance.tiles.remove(removed_tile)
 
-            game_instance.score -= removed_value // 2 
+            game_instance.score -= removed_value // 2
+            play_powerup_sfx("bom")
         else:
             return
     else:
