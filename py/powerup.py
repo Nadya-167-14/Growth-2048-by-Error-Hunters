524acd616106ea38de976a0452d74f5a69c457de
Nadya Shafwah Yusuf <nadya.123140167@student.itera.ac.id>
2025-05-30 23:28:52 +0700

    Update powerup.py

--- py/powerup.py
+++ py/powerup.py
@@ -4,46 +4,56 @@ from animation import BombAnimation, PupukAnimation
 pygame.mixer.init()
 active_animations = []
 
-powerup_sounds = {
-    "pupuk": pygame.mixer.Sound("assets/sfx/pupuk.mp3"),
-    "penyiram_otomatis": pygame.mixer.Sound("assets/sfx/siram.mp3"),
-    "bom": pygame.mixer.Sound("assets/sfx/bom.mp3")
-}
+class PowerUp:
+    sound_path = None
+    def __init__(self, game_instance):
+        self.game = game_instance
+        self.sound = pygame.mixer.Sound(self.sound_path) if self.sound_path else None
 
-def play_powerup_sfx(powerup_name):
-    sound = powerup_sounds.get(powerup_name)
-    if sound:
-        sound.play()
+    def play_sfx(self):
+        if self.sound:
+            self.sound.play()
 
-def pupuk(game_instance, row, col):
-    row = int(row)
-    col = int(col)
-    if 0 <= row < game_instance.grid_size and 0 <= col < game_instance.grid_size:
-        if game_instance.matrix[row][col] is not None:
-            game_instance.matrix[row][col].value *= 2
-            game_instance.score += game_instance.matrix[row][col].value
-            play_powerup_sfx("pupuk")
-            x = game_instance.BOARD_X_OFFSET + col * (game_instance.TILE_SIZE + game_instance.TILE_SPACING) + game_instance.TILE_SPACING
-            y = game_instance.BOARD_Y_OFFSET + row * (game_instance.TILE_SIZE + game_instance.TILE_SPACING) + game_instance.TILE_SPACING
-            active_animations.append(PupukAnimation((x, y)))
+    def activate(self, *args, **kwargs):
+        raise NotImplementedError("Please implement the activate method.")
 
-def penyiram_otomatis(game):
-    game.add_new_tile()
-    game.score += 10
-    play_powerup_sfx("penyiram_otomatis")
+class PupukPowerUp(PowerUp):
+    sound_path = "assets/sfx/pupuk.mp3"
 
-def bom(game_instance, row, col):
-    row = int(row)
-    col = int(col)
-    if 0 <= row < game_instance.grid_size and 0 <= col < game_instance.grid_size:
-        if game_instance.matrix[row][col] is not None:
-            removed_tile = game_instance.matrix[row][col]
-            removed_value = removed_tile.value
-            x = game_instance.BOARD_X_OFFSET + col * (game_instance.TILE_SIZE + game_instance.TILE_SPACING) + game_instance.TILE_SPACING
-            y = game_instance.BOARD_Y_OFFSET + row * (game_instance.TILE_SIZE + game_instance.TILE_SPACING) + game_instance.TILE_SPACING
-            active_animations.append(BombAnimation((x, y)))
-            game_instance.matrix[row][col] = None
-            if removed_tile in game_instance.tiles:
-                game_instance.tiles.remove(removed_tile)
-            game_instance.score -= removed_value // 2
-            play_powerup_sfx("bom")
+    def activate(self, row, col):
+        row, col = int(row), int(col)
+        if 0 <= row < self.game.grid_size and 0 <= col < self.game.grid_size:
+            tile = self.game.matrix[row][col]
+            if tile:
+                tile.value *= 2
+                self.game.score += tile.value
+                self.play_sfx()
+                x = self.game.BOARD_X_OFFSET + col * (self.game.TILE_SIZE + self.game.TILE_SPACING) + self.game.TILE_SPACING
+                y = self.game.BOARD_Y_OFFSET + row * (self.game.TILE_SIZE + self.game.TILE_SPACING) + self.game.TILE_SPACING
+                active_animations.append(PupukAnimation((x, y)))
+
+class PenyiramOtomatisPowerUp(PowerUp):
+    sound_path = "assets/sfx/siram.mp3"
+
+    def activate(self):
+        self.game.add_new_tile()
+        self.game.score += 10
+        self.play_sfx()
+
+class BomPowerUp(PowerUp):
+    sound_path = "assets/sfx/bom.mp3"
+
+    def activate(self, row, col):
+        row, col = int(row), int(col)
+        if 0 <= row < self.game.grid_size and 0 <= col < self.game.grid_size:
+            tile = self.game.matrix[row][col]
+            if tile:
+                removed_value = tile.value
+                x = self.game.BOARD_X_OFFSET + col * (self.game.TILE_SIZE + self.game.TILE_SPACING) + self.game.TILE_SPACING
+                y = self.game.BOARD_Y_OFFSET + row * (self.game.TILE_SIZE + self.game.TILE_SPACING) + self.game.TILE_SPACING
+                active_animations.append(BombAnimation((x, y)))
+                self.game.matrix[row][col] = None
+                if tile in self.game.tiles:
+                    self.game.tiles.remove(tile)
+                self.game.score -= removed_value // 2
+                self.play_sfx()
